11eba7efef14413f8f1b4f1e51e9dabf02a3484a
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-01 17:41:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,10 @@
-if Time.now.to_i % 2 == 0
-  puts "even!"
-else
-  puts "odd!"
+current_time = Time.now 
+current_time = current_time.to_i 
+
+if current_time.even ?
+  puts "even"
+else 
+  puts "odd"
 end
 
 
